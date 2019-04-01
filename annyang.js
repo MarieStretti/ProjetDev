101c79fe8239aa1ab68965ede144b681e66401f2
aschleich <38109180+aschleich@users.noreply.github.com>
2019-04-01 11:54:56 +0200

    Intégration de Annyang
    intégration de l'aide vocale à la page index.html

---
+++ annyang.js
@@ -0,0 +1,19 @@
+
+if (!annyang) {
+  console.log("Speech Recognition is not supported");
+}
+if (annyang) {
+  // Add our commands to annyang
+  annyang.addCommands({
+    'hello': function() { alert('Hello world!'); }
+  });
+
+  // Tell KITT to use annyang
+  SpeechKITT.annyang();
+
+  // Define a stylesheet for KITT to use
+  SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/0.3.0/themes/flat.css');
+
+  // Render KITT's interface
+  SpeechKITT.vroom();
+}
