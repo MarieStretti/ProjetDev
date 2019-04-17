e6c7360b7f67f88595e8ef0f6c1338c29f327071
ridley.campbell_ENSGGEOMATIQUE <ridley.campbell@ensg.eu>
2019-04-17 10:57:21 +0200

    Version Beta finie
    + projection des coordonnées
    + cadre de couleur amélioré
    + commentaires

--- CodeExemple/index.js
+++ CodeExemple/index.js
@@ -1,4 +1,4 @@
-
+// imports pour arcgis online
 require([
   "esri/Map",
   "esri/views/MapView",
@@ -9,16 +9,15 @@ require([
   "esri/Graphic",
   "esri/geometry/Point",
   "dojo/domReady!"
-  
+
 ], function(Map, MapView,FeatureLayer,Query,Polygon,QueryTask, Graphic,Point) {
 
+// creation de la carte
 var map = new Map({
   basemap: "topo-vector"
 });
 
-
-//*** ADD ***//
-// Define a unique value renderer and symbols
+// renderer : creation des attributs de la carte
 var gareRenderer = {
   "type": "unique-value",
   "field": "mode",
@@ -64,18 +63,16 @@ var gares = new FeatureLayer({
   renderer: gareRenderer
 });
 
-
 // Add the layer
 map.add(gares,0);
 
-// creation map
+// creation de la view
 var view = new MapView({
   container: "viewDiv",
   map: map,
   center: [2.34,48.85], //longlats
   zoom: 12
 });
-
 
 /// CADRE DE COULEUR
 var cadreNord  = document.getElementById('cadreNord');
@@ -84,7 +81,20 @@ var cadreEst   = document.getElementById('cadreEst');
 var cadreSud   = document.getElementById('cadreSud');
 
 
-/// on stocke les coordonnées de TOUS LES POINTS
+/// préparation des projections de coordonnées avec proj4
+var wgs84 = proj4.Proj('EPSG:4326');
+console.log(wgs84);
+
+proj4.defs("EPSG:2154","+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
+
+var lambert93 = proj4.Proj("EPSG:2154");
+console.log(lambert93);
+
+var firstProjection = wgs84 ;
+var secondProjection = lambert93;
+
+
+/// stockage des coordonnées de TOUS LES POINTS
 var query = gares.createQuery();
 query.outFields = ["x","y"];
 gares.queryFeatures(query).features;
@@ -102,112 +112,63 @@ view.on(['pointer-down'], function() {
 
   console.log('start!');
 
+  
+////  coordonnées d'emprise actuelle de la carte /////
 
-////  emprise actuelle de la carte 
-  /*
   var size   =  view.size; // emprise en pixels de la carte
-  var width  = size[0]; // longueur de la carte
-  var height = size[1]; // largeur de la carte
-  var x_center = height/2;
-  var y_center = width/2;
+  var width  = size[1]; // longueur de la carte
+  var height = size[0]; // largeur de la carte
+  var pix_x_center = height/2;
+  var pix_y_center = width/2;
 
-  console.log("center: ",x_center, y_center);
-
-
+  console.log("center : ",view.toMap({x: pix_x_center, y:pix_y_center}));
   // dimensions du polygone d'emprise
-  var dx = 0.5*(height/2);
-  var dy = 0.5*(width/2);
+  var dx = 0.8*(height/2);
+  var dy = 0.8*(width/2);
 
   console.log("dx, dy : ",dx, dy);
+  var pix_x_ouest = pix_x_center - dx;
+  var pix_x_est = pix_x_center + dx;
+  var pix_y_nord = pix_y_center - dy; /// pixels != lon/lat !!!!!
+  var pix_y_sud = pix_y_center + dy;
 
+  // coordonnées des 'coins' de la carte en pixels > 80% de l'emprise environ
+  var lat_nord = view.toMap({x: pix_x_est, y:pix_y_nord}).latitude; console.log('lat_nord',lat_nord);
+  var lat_sud = view.toMap({x: pix_x_est, y:pix_y_sud}).latitude; console.log('lat_sud',lat_sud);
+  var lon_ouest = view.toMap({x: pix_x_ouest, y:pix_y_nord}).longitude; console.log('lon_ouest',lon_ouest);
+  var lon_est = view.toMap({x: pix_x_est, y:pix_y_nord}).longitude; console.log('lon_est',lon_est);
 
-  var x_ouest = x_center - dx;
-  var x_est = x_center + dx;
-  var y_nord = y_center - dy; /// pixels != lon/lat !!!!!
-  var y_sud = y_center + dy;
-
-  var lat_nord = view.toMap({x: x_est, y:y_nord}).latitude;
-  var lat_sud = view.toMap({x: x_est, y:y_sud}).latitude;
-  var lon_ouest = view.toMap({x: x_ouest, y:y_nord}).longitude;
-  var lon_est = view.toMap({x: x_est, y:y_nord}).longitude;
-*/
-
-var lon_ouest = 2.282; // COORDONNEES A CONVERTIR ( rentrées à la main )
-var lat_sud = 48.830;
-var lon_est = 2.313;
-var lat_nord = 48.890;
-
-//// CONVERSION DE COORDONNES AVEC PROJ4 //////
-var wgs84 = proj4.Proj('EPSG:4326');
-console.log(wgs84);
-
-//proj4.defs('EPSG:27571','+proj=lcc +lat_1=49.50000000000001 +lat_0=49.50000000000001 +lon_0=0 +k_0=0.999877341 +x_0=600000 +y_0=1200000 +a=6378249.2 +b=6356515 +towgs84=-168,-60,320,0,0,0,0 +pm=paris +units=m +no_defs');
-
-proj4.defs("EPSG:2154","+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
-
-
-var lambert1 = proj4.Proj("EPSG:2154");
-console.log(lambert1);
-
-
-var firstProjection = wgs84 ;
-var secondProjection = lambert1;
-
-var new_coord1 = proj4(firstProjection,secondProjection,[lon_ouest,lat_sud]); // lon lat
-var new_coord2 = proj4(firstProjection,secondProjection,[lon_est,lat_nord]);
-
-var new_coord3 = proj4(firstProjection,secondProjection).inverse(new_coord1);
-var new_coord4 = proj4(firstProjection,secondProjection).inverse(new_coord2);
-
-console.log(new_coord1, new_coord2);
-console.log(new_coord3, new_coord4);
-
-
-////////////////////////////// fin de conversion proj4 /////////:::
-
-
-console.log("lat_N : ",lat_nord);
-console.log("lat_S : ",lat_sud);
-console.log("lon_E : ",lon_est);
-console.log("lon_O : ",lon_ouest);
-
-
-var p1 = proj4.Proj('EPSG:27571');
-var p2 = proj4.Proj('EPSG:4326');
-
-console.log(p1,p2);
-
-proj4(firstProjection,[-71,41]);
-
-var y_nord  = projectedPoints[0];
-var y_sud   = projectedPoints[1];
-var x_est   = projectedPoints[2];
-var x_ouest = projectedPoints[3];
-
-
-/* comment trouver les coordonnées du centre de la carte ???
   // étendue de l'emprise de la carte
-  var x_ouest = view.extent.xmin;
-  var y_sud = view.extent.ymin;
-  var x_est = view.extent.xmax;
-  var y_nord = view.extent.ymax;
+  var view_x_ouest = view.extent.xmin; ///// lambert 93?
+  var view_y_sud = view.extent.ymin;
+  var view_x_est = view.extent.xmax;
+  var view_y_nord = view.extent.ymax;
+  console.log("###### Emprise de la view ######");
+  console.log("O > ",view_x_ouest);
+  console.log("E > ",view_x_est);
+  console.log("N > ",view_y_nord);
+  console.log("S > ",view_y_sud);
 
-  console.log("O > ",x_ouest);
-  console.log("E > ",x_est);
-  console.log("N > ",y_nord);
-  console.log("S > ",y_sud);
+//// CONVERSION DE COORDONNES D'EMPRISE AVEC PROJ4 //////
 
-  */
+  var new_coord1 = proj4(firstProjection,secondProjection,[lon_ouest,lat_sud]); // lon lat : point sud ouest
+  var new_coord2 = proj4(firstProjection,secondProjection,[lon_est,lat_nord]); // lon lat : point nord est
 
-  // conteneurs du nombre des points dans chaque direction
+  var y_nord  = new_coord2[1];
+  var x_est   = new_coord2[0];
+  var y_sud   = new_coord1[1];
+  var x_ouest = new_coord1[0];
+
+  // initialisation des conteneurs du nombre des points dans chaque direction
   var points_NSEO = [0,0,0,0];
   var cadres_NSEO = [cadreNord, cadreSud, cadreEst,cadreOuest];
 
-//////// DETECTION DE LA POSITION DES POINTS     /////////
-  for (var i = 0; i < liste_points.length; i++) {  
 
-    var x = liste_points[i][0]; /// EN LAMBERT 1
-    var y = liste_points[i][1]; /// EN LAMBERT 1
+//////// DETECTION DE LA POSITION DES POINTS     /////////
+  for (var i = 0; i < liste_points.length; i++) {
+
+    var x = liste_points[i][0]; /// EN LAMBERT 93
+    var y = liste_points[i][1]; /// EN LAMBERT 93
 
     if (x < x_ouest){ // point à gauche
       points_NSEO[3] +=1;
@@ -215,38 +176,44 @@ var x_ouest = projectedPoints[3];
     if (x > x_est){ // point à droite
       points_NSEO[2] +=1;
     }
-    if (y < y_nord){ // point en haut
+    if (y > y_nord){ // point en haut
       points_NSEO[0] +=1;
     }
     if (y < y_sud){ // point en bas
       points_NSEO[1] +=1;
     }
   }
-
+  console.log("####### nombre de points détectés dans les quadrants #######")
   console.log("N : ",points_NSEO[0]);
   console.log("S : ",points_NSEO[1]);
   console.log("E : ",points_NSEO[2]);
   console.log("O : ",points_NSEO[3]);
+  console.log("total : ",points_NSEO[3]+points_NSEO[1]+points_NSEO[2]+points_NSEO[0])
 
 
   //////// modification de la couleur des cadres   //////////////////:
-  
+  var couleurs_croissantes = ["lightgrey","lightgreen","gold","orange","red","pink","purple","blue","darkblue"];
+
   for (var i = 0; i<4 ; i++){
     cadres_NSEO[i].innerHTML = points_NSEO[i];
-    if (points_NSEO[i] >= 500){
-      cadres_NSEO[i].style.background = "purple";
+    if (points_NSEO[i] >= 700){
+      cadres_NSEO[i].style.background = couleurs_croissantes[8];
+    }else if (points_NSEO[i] >= 600){
+      cadres_NSEO[i].style.background = couleurs_croissantes[7];
+    }else if (points_NSEO[i] >= 500){
+      cadres_NSEO[i].style.background = couleurs_croissantes[6];
     }else if (points_NSEO[i] >= 400){
-      cadres_NSEO[i].style.background = "pink";
+      cadres_NSEO[i].style.background = couleurs_croissantes[5];
     }else if (points_NSEO[i] >= 300){
-      cadres_NSEO[i].style.background = "red";
+      cadres_NSEO[i].style.background = couleurs_croissantes[4];
     }else if (points_NSEO[i] >= 200){
-      cadres_NSEO[i].style.background = "orange";
+      cadres_NSEO[i].style.background = couleurs_croissantes[3];
     }else if (points_NSEO[i] >= 100){
-      cadres_NSEO[i].style.background = "gold";
+      cadres_NSEO[i].style.background = couleurs_croissantes[2];
     }else if (points_NSEO[i] >= 50){
-      cadres_NSEO[i].style.background = "lightgreen";
+      cadres_NSEO[i].style.background = couleurs_croissantes[1];
     }else{
-      cadres_NSEO[i].style.background = "lightgrey";
+      cadres_NSEO[i].style.background = couleurs_croissantes[0];
     };
   };
   console.log('bg : ',cadreNord.style.background);
